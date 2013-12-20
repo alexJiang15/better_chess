8df2a14540f891c4ba03c0b47c0e185ec5b314c5
Alex Jiang <dashuai99@gmail.com>
2013-12-19 20:05:18 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BetterChess::Application.config.secret_token = '6fb2c8aaf7ed37962913145dc9ccba151ff8842d6be2dab34af4f7876d55e9341505363904c6372a33055f5d433874d0ba117c5564caffa9748d93d88a09511a'
