6cdff1a0cdc5f2b87ea645cb7a1940dbc2da722a
Alexander Jiang <dashuai99@gmail.com>
2014-04-16 10:33:56 -0400

    start over with rails new --skip-test-unit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
-  include SessionsHelper
-
-  # Force signout to prevent CSRF attacks
-  def handle_unverified_request
-    sign_out
-    super
-  end
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
 end
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-BetterChess::Application.config.secret_token = '6fb2c8aaf7ed37962913145dc9ccba151ff8842d6be2dab34af4f7876d55e9341505363904c6372a33055f5d433874d0ba117c5564caffa9748d93d88a09511a'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BetterChess::Application.config.secret_key_base = 'a4e3b9131528e873d72165419225b3cd5f8cb457c354e63da623257bc387cdc417b89550e3b4f02cb6cb83674eba8aba8c20c096b68e3b86fed070faeddae86f'
